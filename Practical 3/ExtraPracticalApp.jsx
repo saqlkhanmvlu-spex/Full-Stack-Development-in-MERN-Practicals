1faa632ea28d840e508b7d8360c95c714804d1ad
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-15 00:29:04 -0700

    Create ExtraPracticalApp.jsx

---
+++ Practical 3/ExtraPracticalApp.jsx
@@ -0,0 +1,19 @@
+import AttendanceDashboard from "./components/AttendanceDashboard";
+import Saqlain from "./components/Saqlain";
+import Ritesh from "./components/Ritesh";
+import Himani from "./components/Himani";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceDashboard />
+      <Saqlain />
+      <Ritesh />
+      <Himani />
+      <Footer />
+    </div>
+  );
+}
+export default App;
