742af141f92a331e31399bb82af11b3901192110
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-15 00:15:20 -0700

    Components of this practical is in components folder

---
+++ Practical 3/Prac3E.jsx
@@ -0,0 +1,15 @@
+import Greeting from "./components/Greeting.jsx";
+import Student from "./components/Student.jsx";
+import Course from "./components/Course.jsx";
+import Marks from "./components/Marks.jsx";
+function App() {
+  return (
+    <div>
+      <Greeting />
+      <Student />
+      <Course />
+      <Marks />
+    </div>
+  );
+}
+export default App;
