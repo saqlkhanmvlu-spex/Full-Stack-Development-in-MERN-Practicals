88b5135185c4d194e5472cf7de1503c7a2fdb805
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-14 23:49:04 -0700

    Create Prac3D.jsx

---
+++ Practical 3/Prac3D.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  return(
+    <div className="container">
+      <h1>Student Dashboard</h1>
+      <hr />
+      <p>Name : Saqlain Khan</p>
+      <p>Roll Number : T013</p>
+      <p>Course : MERN</p>
+      <p>Semester : V</p>
+      <button>View Button</button>
+    </div>
+  );
+}
+export default App;
