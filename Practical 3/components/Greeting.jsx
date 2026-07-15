8eb3c75b9b12118aafb82250dbed4273023bde96
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-15 00:13:46 -0700

    Add files via upload

---
+++ Practical 3/components/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return (
+    <div className="card">
+      <h2>Greeting</h2>
+      <p>Welcome to the Student Dashboard.</p>
+    </div>
+  );
+}
+export default Greeting;
---
+++ Practical 3/components/Marks.jsx
@@ -0,0 +1,11 @@
+function Marks() {
+  return (
+    <div className="card">
+      <h2>Marks</h2>
+      <p>React: 90</p>
+      <p>Java: 88</p>
+      <p>Python: 95</p>
+    </div>
+  );
+}
+export default Marks;
