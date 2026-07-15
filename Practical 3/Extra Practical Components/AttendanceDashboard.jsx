1653e8de513bf8265b1012880423e53a5522717f
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-15 00:27:39 -0700

    Create AttendanceDashboard.jsx

---
+++ Practical 3/Extra Practical Components/AttendanceDashboard.jsx
@@ -0,0 +1,9 @@
+function AttendanceDashboard() {
+  return (
+    <div className="header">
+      <h1>Student Attendance Dashboard</h1>
+      <p>Semester V - MERN Batch</p>
+    </div>
+  );
+}
+export default AttendanceDashboard;
