f1d0195e37f0f3c04a2f82e750a410ad82228a44
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-07-15 00:13:16 -0700

    Create Course.jsx

---
+++ Practical 3/components/Course.jsx
@@ -0,0 +1,10 @@
+function Course() {
+  return (
+    <div className="card">
+      <h2>Course</h2>
+      <p>Course: MERN Stack</p>
+      <p>Semester: V</p>
+    </div>
+  );
+}
+export default Course;
