9ebd0258da0f205d14f6762f0d7adce285abbe7b
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-09 11:28:24 +0530

    Profile.jsx

---
+++ Practical 5/Practical 5C/Profile.jsx
@@ -0,0 +1,12 @@
+function Profile(){
+   return(
+       <div>
+           <h1>Student Profile</h1>
+           <hr />
+           <p>Name: Himani Malankar</p>
+           <p>CGPA: 8.95</p>
+           <p>Skills: Data Science , Data Analytics</p>
+       </div>
+   )
+}
+export default Profile;
