7e24e696afc6b9d9b993ed2245810b0cb7a3fb25
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-09 11:26:08 +0530

    About.jsx

---
+++ Practical 5/Practical 5B/About.jsx
@@ -0,0 +1,9 @@
+function About(){
+return (
+<div>
+            <h1>About page</h1>
+            <p>This page contains information about our application Ritesh Chaubey</p>
+</div>
+   	 )
+ }
+export default About;
