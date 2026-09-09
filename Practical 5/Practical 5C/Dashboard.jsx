1ce873151347b43818718a727864aae2e4072b37
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-09 11:28:06 +0530

    Dashboard.jsx

---
+++ Practical 5/Practical 5C/Dashboard.jsx
@@ -0,0 +1,14 @@
+function Dashboard(){
+   return(
+       <div>
+           <h1>Placement Dashboard</h1>
+           <p>Welcome to Student Placement Portal.</p>
+           <h2>Placement Summary</h2>
+           <p>Total Companies: 25</p>
+           <p>Students Placed: 120</p>
+           <p>Highest Package: 12LPA</p>
+       </div>
+   )
+}
+export default Dashboard;
+
