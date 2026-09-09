d6990c7aced985ba84d919b29c7c13783b646f64
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-09 11:29:04 +0530

    Statistics.jsx

---
+++ Practical 5/Practical 5C/Statistics.jsx
@@ -0,0 +1,13 @@
+function Statistics(){
+   return(
+       <div>
+           <h1>Placement Statistics</h1>
+           <hr />
+           <p>Students Registered: 200</p>
+           <p>Student Placed: 120</p>
+           <p>Placement Percentage: 60%</p>
+           <p>Average Package: ₹5.5LPA</p>
+       </div>
+   )
+}
+export default Statistics;
