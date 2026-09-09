73509d40c4919c591549379c1e6171884441c58c
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-09-09 11:28:42 +0530

    Job.jsx

---
+++ Practical 5/Practical 5C/Job.jsx
@@ -0,0 +1,24 @@
+function Jobs(){
+   return(
+       <div>
+           <h1>Job Opportunities</h1>
+           <hr />
+           <h2>Software Developer</h2>
+           <p>Company: ABC Technologies</p>
+           <p>Packages: ₹6LPA</p>
+           <hr />
+           <h2>Web Developer</h2>
+           <p>Company: XYZ Technologies</p>
+           <p>Packages: ₹5LPA</p>
+           <hr />
+           <h2>Data Scientist</h2>
+           <p>Company: Entrata Technologies</p>
+           <p>Packages: ₹15LPA</p>
+           <hr />
+           <h2>Data Analyst</h2>
+           <p>Company: Accenture Technologies</p>
+           <p>Packages: ₹12LPA</p>
+       </div>
+   )
+}
+export default Jobs;
